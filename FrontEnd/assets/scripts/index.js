52f274231d7819628abdb3e6fc56007f7d5f97c0
Jean-Luc <eltoucandepaitilla@gmail.com>
2024-07-27 13:42:47 +0200

    Etape 1: Récupération des API, récupération du DOM.

---
+++ FrontEnd/assets/scripts/index.js
@@ -0,0 +1,27 @@
+//####################################################################################
+//RECUPERATION DES API
+const apiUrl = "http://localhost:5678/api";
+const getWorksUrl = `${apiUrl}/works`;
+const getCategoriesUrl = `${apiUrl}/categories`;
+const loginUrl = `${apiUrl}/users/login`;
+
+//####################################################################################
+//RECUPERATION DU DOM
+document.addEventListener("DOMContentLoaded", async function () {
+  console.log("Document is ready");
+
+//##################################################################################
+//CREATION DE LA GALERIE DANS LAQUELLE S'AFFICHERONT LES PROJETS
+
+
+
+
+
+
+
+
+
+
+
+
+}); // Fin de document.addEventListener("DOMContentLoaded", async function () {
