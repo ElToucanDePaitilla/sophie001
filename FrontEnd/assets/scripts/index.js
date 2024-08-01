28d615e9fcf65a3481f1a9f24cf720f444c16a0a
Jean-Luc <eltoucandepaitilla@gmail.com>
2024-08-01 17:23:19 +0200

    Etape 3.2 : deleteGallery mise en place, boutons en place, reste à ajouter la fonction delete aux icones trash-can.

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -268,7 +268,82 @@ window.addEventListener("load", function() {
 });
 
 
+//####################################################################################
+//GALERIE DELETE
+//####################################################################################
 
+//RECUPERATION DES "IMAGEURLS" DEPUIS l'API 
+
+async function fetchWorks() {
+  try {
+    const response = await fetch(getWorksUrl);
+    const works = await response.json();
+    console.log("Works:", works);
+
+    const imageUrls = works.map(work => work.imageUrl);
+    console.log("Image URLs:", imageUrls);
+    return imageUrls;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des imageUrls:", error);
+    return [];
+  }
+}
+
+fetchWorks()
+
+//####################################################################################
+//INITIALISATION DE LA GALERIE EN EN VIDANT SON CONTENU
+
+function clearDeleteGallery() {
+    const gallery = document.getElementById('delete-gallery');
+    while (gallery.firstChild) {
+        gallery.removeChild(gallery.firstChild);
+    }
+    console.log("#delete-gallery has been cleared");
+}
+
+// Appel de la fonction pour vider la galerie
+clearDeleteGallery();
+
+
+//####################################################################################
+//CREATION DES ELEMENTS DE LA GALERIE
+
+function createGalleryItems(imageUrls) {
+    const gallery = document.getElementById('delete-gallery');
+    imageUrls.forEach(url => {
+        const elementFusionne = document.createElement('div');
+        elementFusionne.id = 'element-fusionne';
+
+        const img = document.createElement('img');
+        img.className = 'images';
+        img.src = url;
+        img.alt = 'Image';
+
+        const fondIcone = document.createElement('div');
+        fondIcone.className = 'fond-icone';
+
+        const icon = document.createElement('i');
+        icon.className = 'fa-solid fa-trash-can icone';
+        icon.style.color = '#ffffff';
+
+        fondIcone.appendChild(icon);
+        elementFusionne.appendChild(img);
+        elementFusionne.appendChild(fondIcone);
+        gallery.appendChild(elementFusionne);
+
+        console.log("Created element:", elementFusionne);
+    });
+}
+
+
+        async function initGallery() {
+        const imageUrls = await fetchWorks();
+        clearDeleteGallery();
+        createGalleryItems(imageUrls);
+        }
+
+        initGallery();
 
 
 
